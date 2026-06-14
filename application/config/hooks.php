3fc0caf3f9960254bd2a9ccf731cc11827cfd869
Jose Lujan <lujan_exogenesis@hotmail.com>
2026-06-14 16:19:25 -0500

    Archivos innecesario de la API login

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,8 +11,3 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
-$hook['pre_system'][] = array(
-    'function'  => 'desactivar_libs_api',
-    'filename'  => 'Api_Hook.php',
-    'filepath'  => 'hooks'
-);
